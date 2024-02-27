d81638478fdcfb3c8563e736b2847f9e1ae7905c
Sean Ippolito <ippolitosean@gmail.com>
2024-02-27 07:11:58 -0800

    first 2 days

---
+++ day-one/band-name-generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator.")
+city = input("Which city were you born in?\n")
+pet = input("What is your pets name?\nHowl")
+print(f"Your band name could be {city} {pet}")
+ 
